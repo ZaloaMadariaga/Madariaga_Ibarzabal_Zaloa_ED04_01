0aa4edb619a8c80c99e310a44c4c3cafd193ce3e
ZaloaMadariaga <zmadariaga@birt.eus>
2024-05-17 20:45:10 +0200

    Versión 3. He encapsulado los cuatro atributos de la clase CCuenta. He eliminado los métodos innecesarios y he cambiado el nombre del método "estado" por "getSaldo"

--- src/main/java/com/mycompany/ud4_te1/CCuenta.java
+++ src/main/java/com/mycompany/ud4_te1/CCuenta.java
@@ -7,46 +7,33 @@ public class CCuenta {
     private double saldo;
     private double tipoInteres;
 
-
-    public CCuenta() {
+    /**
+     * @return the cuenta
+     */
+    public String getCuenta() {
+        return cuenta;
     }
 
-    public CCuenta(String nom, String cue, double sal, double tipo) {
-        nombre = nom;
-        cuenta = cue;
-        saldo = sal;
-        tipoInteres = tipo;
+    /**
+     * @param cuenta the cuenta to set
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
     }
 
-    public void asignarNombre(String nom) {
-        setNombre(nom);
-    }
+    /**
+     * @return the saldo
+     */
 
-    public String obtenerNombre() {
-        return getNombre();
-    }
-
-    public double estado() {
+    public double getSaldo() {
         return saldo;
     }
 
-    public void ingresar(double cantidad) throws Exception {
-        if (cantidad < 0) {
-            throw new Exception("No se puede ingresar una cantidad negativa");   
-        }
-        saldo = saldo + cantidad;
-    }
-
-    public void retirar(double cantidad) throws Exception {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado() < cantidad + 2)
-            throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - (cantidad + 2);
-    }
-
-    public String obtenerCuenta() {
-        return cuenta;
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
     }
 
     public String getNombre() {
@@ -64,4 +51,34 @@ public class CCuenta {
     public void setTipoInteres(double tipoInteres) {
         this.tipoInteres = tipoInteres;
     }
+
+    public CCuenta() {
+    }
+
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
+        tipoInteres = tipo;
+    }
+
+
+
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0) {
+            throw new Exception("No se puede ingresar una cantidad negativa");   
+        }
+        saldo = saldo + cantidad;
+    }
+
+    public void retirar(double cantidad) throws Exception {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (getSaldo() < cantidad + 2)
+            throw new Exception ("No se hay suficiente saldo");
+        saldo = saldo - (cantidad + 2);
+    }
+
+
+
 }
--- src/main/java/com/mycompany/ud4_te1/Main.java
+++ src/main/java/com/mycompany/ud4_te1/Main.java
@@ -13,19 +13,19 @@ public class Main {
 
     private static void operativaCuenta(CCuenta cuentaTrabajo) {
         double saldoActual;
-        saldoActual = cuentaTrabajo.estado();
+        saldoActual = cuentaTrabajo.getSaldo();
         System.out.println("El saldo actual es " + saldoActual);
         try {
             System.out.println("Cargo en cuenta");
             cuentaTrabajo.retirar(2300);
-            System.out.println("El nuevo saldo es: " + cuentaTrabajo.estado());
+            System.out.println("El nuevo saldo es: " + cuentaTrabajo.getSaldo());
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Abono en cuenta");
             cuentaTrabajo.ingresar(695);
-            System.out.println("El nuevo saldo es: " + cuentaTrabajo.estado());
+            System.out.println("El nuevo saldo es: " + cuentaTrabajo.getSaldo());
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
